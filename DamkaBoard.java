349b20c8a898b1d884f7281899433a6ac4500e3c
Tom Ben Ezer <tom.benezer@runi.ac.il>
2023-12-28 09:21:16 +0200

    fix board

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -8,8 +8,8 @@ public class DamkaBoard {
 		for(int i = 0; i < n; i++) {
 			System.out.println();
 			for(int j = 0; j < n; j++) {
-				if((i % 2 == 1) && j == 0)
-					System.out.print(" * ");
+				if((i % 2 == 1))
+					System.out.print(" *");
 				else
 					System.out.print("* ");
 			}
